2a765ff1026ee96776d7208834acdb23030489d0
vignejerome <vignejerome@free.fr>
2014-02-28 23:27:22 +0100

    Personnalisation de la vue : extensions Twig, barre latérale et Assetic

--- app/DoctrineMigrations/Version20140228214051.php
+++ app/DoctrineMigrations/Version20140228214051.php
@@ -8,13 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20140224201201 extends AbstractMigration
+class Version20140228214051 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
+        $this->addSql("ALTER TABLE blog ADD slug VARCHAR(255) NOT NULL");
     }
 
     public function down(Schema $schema)
@@ -22,5 +23,6 @@ class Version20140224201201 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
+        $this->addSql("ALTER TABLE blog DROP slug");
     }
 }
